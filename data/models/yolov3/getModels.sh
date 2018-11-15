8ae1414433cb78534396daafff7026361c8780d3
panjq1 <panjq1@lenovocom>
2018-11-15 09:50:28 +0800

    ADD YOLO v3 models

---
+++ data/models/yolov3/getModels.sh
@@ -0,0 +1,6 @@
+# https://github.com/spmallick/learnopencv/tree/master/ObjectDetection-YOLO
+# https://blog.csdn.net/haoqimao_hard/article/details/82081285
+
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O ./yolov3.cfg
+wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O ./coco.names
